cb9ee21d92e98776b237256b7161b9fa2fd96e97
Maria Antipova <experiment0@yandex.ru>
2025-08-30 03:11:37 +0300

    Refactoring the user greeting

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,15 @@
+from typing import Optional
 import prompt
 
 
-def welcome_user():
-    name = prompt.string("May I have your name? ")
-    print(f"Hello, {name}!")
+def welcome_user() -> Optional[str]:
+    """Приветствует пользователя и возвращает его имя
+
+    Returns:
+        Optional[str]: имя пользователя
+    """
+    print("Welcome to the Brain Games!")    
+    user_name = prompt.string("May I have your name? ")    
+    print(f"Hello, {user_name}!")
+    
+    return user_name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
 from brain_games.cli import welcome_user
 
 
-def main():
-    print("Welcome to the Brain Games!")
-    welcome_user()
+def main() -> None:
+    """Точка входа в проект
+    """
+    user_name = welcome_user()
 
 
 if __name__ == "__main__":
